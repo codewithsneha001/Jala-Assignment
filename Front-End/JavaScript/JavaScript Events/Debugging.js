842b7bfd9111343f5400ccc25c6df6895d39e813
codewithsneha001 <100217905+codewithsneha001@users.noreply.github.com>
2025-05-21 15:39:40 +0530

    Add files via upload

---
+++ Front-End/JavaScript/JavaScript Events/Debugging.js
@@ -0,0 +1,9 @@
+function calculateSum(a, b) {
+    console.log("Input a:", a); // Debugging input
+    console.log("Input b:", b);
+    let sum = a + b;
+    console.log("Calculated sum:", sum); // Debugging output
+    return sum;
+}
+
+calculateSum(5, 10);
