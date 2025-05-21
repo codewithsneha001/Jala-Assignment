75de9c9ef3e9bb626be636a181187f8e031932d5
codewithsneha001 <100217905+codewithsneha001@users.noreply.github.com>
2025-05-21 15:10:00 +0530

    Add files via upload

---
+++ Front-End/JavaScript/JavaScript Events/JSON.js
@@ -0,0 +1,9 @@
+const student = {
+    name: "John",
+    age: 21,
+    course: "Computer Science"
+};
+
+console.log(student.name); // John
+console.log(student.age); // 21
+console.log(student.course); // Computer Science
