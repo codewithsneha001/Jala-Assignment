91d0df2beb00b4b800ed261277976ccfe8e44da4
codewithsneha001 <100217905+codewithsneha001@users.noreply.github.com>
2025-05-21 15:38:35 +0530

    Delete Front-End/JavaScript/JavaScript Events directory

--- Front-End/JavaScript/JavaScript Events/Debugging.js
+++
@@ -1,9 +0,0 @@
-function calculateSum(a, b) {
-    console.log("Input a:", a); // Debugging input
-    console.log("Input b:", b);
-    let sum = a + b;
-    console.log("Calculated sum:", sum); // Debugging output
-    return sum;
-}
-
-calculateSum(5, 10);
